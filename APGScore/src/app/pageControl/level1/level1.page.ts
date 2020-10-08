704623873e7f9aca31e180a81ae597c208301703
fame <chutipong.sa.60@ubu.ac.th>
2020-10-08 17:39:33 +0700

    edit the 1st state

--- APGScore/src/app/pageControl/level1/level1.page.ts
+++ APGScore/src/app/pageControl/level1/level1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { QUESTION } from 'src/app/mock/mock-question';
 import { Question } from 'src/app/model/question';
+import { QUESTION1, QUESTION2 } from './../../mock/mock-question';
 
 @Component({
   selector: 'app-level1',
@@ -9,32 +9,22 @@ import { Question } from 'src/app/model/question';
 })
 export class Level1Page implements OnInit {
 
-  cards: Question[] = QUESTION;
-  randomCard: Question[] = [];
+  quiz: Question[];
   status = '';
   score = 0;
+  random = 0;
 
   constructor() { }
 
   ngOnInit() {
-    let rd = Math.floor(Math.random() * this.cards.length);
-    this.randomCard.push(this.cards[rd]);
-    console.log('cards0 = ', this.cards[rd].id);
-    for (let index = 1; index < 100; index++) {
-      if (this.randomCard.length === 5) {
-        console.log('random = ', this.randomCard);
-        break;
-      } else {
-        let rd = Math.floor(Math.random() * this.cards.length);
-        console.log(this.randomCard.findIndex(std=> std.id === this.cards[rd].id) === -1);
-        console.log(this.randomCard.findIndex(std=> std.id === this.cards[rd].id));
-        if (this.randomCard.findIndex(std=> std.id === this.cards[rd].id) !== -1) {
-          console.log('ไม่ต้อง push');
-        } else {
-          this.randomCard.push(this.cards[rd]);
-        }
-      }
+    this.random = Math.floor(Math.random() * 2) + 1;
+    console.log('rd =', this.random);
+    if (this.random === 1) {
+      this.quiz = QUESTION1;
+    } else if (this.random === 2) {
+      this.quiz = QUESTION2;
     }
+    console.log('quiz =', this.quiz);
   }
 
   onCheck(str: string) {
@@ -45,19 +35,17 @@ export class Level1Page implements OnInit {
 
   next(i) {
     console.log('OK status:', this.status);
-    if (this.randomCard[i].answer === this.status) {
+    if (this.quiz[i].answer === this.status) {
       this.score += 1;
       console.log('score =', this.score);
     } else {
       this.score += 0;
       console.log('score =', this.score);
     }
-    this.randomCard.splice(0, 1);
-    console.log('count =', this.randomCard.length);
-    console.log('catd =', this.randomCard);
+    this.quiz.splice(0, 1);
+    console.log('count =', this.quiz.length);
+    console.log('catd =', this.quiz);
     this.status = '';
   }
 
 }
-
-
