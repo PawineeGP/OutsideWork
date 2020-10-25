0b69a12cd01303352e81812655669fd6324e0045
Pawinee Purachanai <pawinee.pu@outlook.com>
2020-10-25 09:02:43 +0700

    update

--- APGScore/src/app/pageControl/view-score/view-score.page.ts
+++ APGScore/src/app/pageControl/view-score/view-score.page.ts
@@ -15,98 +15,43 @@ export class ViewScorePage implements OnInit {
   public rows: any;
   tablestyle = ' bootstrap';
   userlist = [];
+  table:any =[];
 
   constructor(private myapi: ServiceApiService) {
 
-    this.columns = [
-      { name: 'Name' },
-      { name: 'Company' },
-      { name: 'Genre' }
-    ];
 
-    this.rows = [
-      {
-        "name": "Escape Room",
-        "company": "Columbia Pictures",
-        "genre": "Horror"
-      },
-      {
-        "name": "Rust Creek",
-        "company": "IFC Films",
-        "genre": "Drama"
-      },
-      {
-        "name": "American Hangman",
-        "company": "Hangman Productions",
-        "genre": "Thriller"
-      },
-      {
-        "name": "The Upside",
-        "company": "STX Entertainment",
-        "genre": "Comedy"
-      },
-      {
-        "name": "Replicas",
-        "company": "Entertainment Studios",
-        "genre": "Sci-Fi"
-      },
-      {
-        "name": "After Darkness",
-        "company": "Grindstone Group",
-        "genre": "Drama"
-      },
-      {
-        "name": "Glass",
-        "company": "Universal Pictures",
-        "genre": "Superhero"
-      },
-      {
-        "name": "Close",
-        "company": "Netflix",
-        "genre": "Action"
-      },
-      {
-        "name": "The Final Wish",
-        "company": "BondIt Capital",
-        "genre": "Horror"
-      },
-      {
-        "name": "Serenity",
-        "company": "Aviron Pictures",
-        "genre": "Drama"
-      },
-      {
-        "name": "Miss Bala",
-        "company": "Columbia Pictures",
-        "genre": "Thriller"
-      },
-      {
-        "name": "Velvet Buzzsaw",
-        "company": "Netflix",
-        "genre": "Comedy"
-      }
-    ];
   }
 
   ngOnInit() {
-    // read data from database
+     
+    this.columns = [
+      { name: 'รหัส',prop: 'id' },
+      { name: 'ชื่อ-สกุล',prop:'name' },      
+      { name: 'ด่าน 1',prop:'state1' },
+      { name: 'ด่าน 2',prop:'state2' },
+      { name: 'ด่าน 3',prop:'state3' },
+      { name: 'คะแนนรวม', prop:'total'}
+
+    ];
     this.myapi.Readdata().subscribe(data => {
       this.userlist = data.map(e => {
-        return {
-          id: e.payload.doc.id,
-          isEdit: false,
-          myuid: e.payload.doc.data()['UID'.toString()],
-          myid: e.payload.doc.data()['id'.toString()],
-          myname: e.payload.doc.data()['name'.toString()],
-          mysurname: e.payload.doc.data()['surname'.toString()],
-          mystate1: e.payload.doc.data()['state1'.toString()],
-          mystate2: e.payload.doc.data()['state2'.toString()],
-          mystate3: e.payload.doc.data()['state3'.toString()],
-          mytotal: e.payload.doc.data()['total'.toString()],
+        return {                  
+          id: e.payload.doc.data()['id'.toString()],
+          name: e.payload.doc.data()['name'.toString()] +' '+ e.payload.doc.data()['surname'.toString()],
+          
+          state1: e.payload.doc.data()['state1'.toString()],
+          state2: e.payload.doc.data()['state2'.toString()],
+          state3: e.payload.doc.data()['state3'.toString()],
+          total: e.payload.doc.data()['total'.toString()],
         };
-      });
-      console.log('userlist =', this.userlist);
+      });   
+      console.log(this.userlist);
+      
+      this.rows = this.userlist;         
     });
+    // read data from database
   }
 
+
+ 
 }
