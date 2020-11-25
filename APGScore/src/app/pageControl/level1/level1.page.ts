9c96ecff8c4411cc0a516fb4ba942d10c02c8c84
chutipong1998 <chutipong.sa.60@ubu.ac.th>
2020-11-25 22:02:33 +0700

    update

--- APGScore/src/app/pageControl/level1/level1.page.ts
+++ APGScore/src/app/pageControl/level1/level1.page.ts
@@ -56,9 +56,8 @@ export class Level1Page implements OnInit {
 
     this.platform.backButton.subscribeWithPriority(1000000, () => {
       if (this.constructor.name == 'Level1Page') {
-        if (window.confirm('do you want to exit app.')) {
-          navigator['app'].exitApp();
-        }
+        clearInterval(this.timer);
+        this.route.navigate([`chooes-level`], { replaceUrl: true });
       }
     });
 
@@ -82,47 +81,9 @@ export class Level1Page implements OnInit {
       this.index = this.userlist.findIndex(std => std.myuid === this.uid);
       console.log('index vel1', this.index);
       // clearInterval(setTimeout( x => {}));
-      this.StartTimer(this.maxtime);
     });
+    this.StartTimer(this.maxtime);
   }
-
-  // StartTimer1(maxtime: any) {
-  //   this.timer = setTimeout(x => {
-  //     // if (this.maxtime <= 0) { }
-  //     // this.maxtime -= 1;
-
-  //     if (maxtime > 0) {
-  //       maxtime -= 1;
-  //       this.maxtime = maxtime;
-  //     } else if (maxtime === 0) {
-  //       this.maxtime = 0;
-  //       return;
-  //     }
-
-  //     if (maxtime >= 0) {
-  //       this.hidevalue = false;
-  //       if (maxtime > 0) {
-  //         this.StartTimer(maxtime);
-  //       } else if (maxtime === 0) {
-  //         this.next(0);
-  //       }
-  //     }
-
-  //     else {
-  //       this.hidevalue = true;
-  //     }
-
-  //   }, 1000);
-  //   // if (this.reload_page == true) {
-  //   //   clearInterval(this.timer);
-  //   //   this.reload_page = false;
-  //   //   console.log('reload_page', this.reload_page);
-  //   //   this.StartTimer(maxtime);
-  //   // } else {
-  //   // console.log('โหลดเสร็จแล้ว');
-  //   // }
-  //   console.log('Timer Maxtime  = ', maxtime);
-  // }
 
   onCheck(str: string) {
     console.log('*****************');
@@ -229,8 +190,6 @@ export class Level1Page implements OnInit {
     console.log('this.quiz.length =', this.quiz.length);
     if (this.quiz.length != 0) {
       this.timer = setTimeout(x => {
-        // if (this.maxtime <= 0) { }
-        // this.maxtime -= 1;
 
         if (maxtime > 0) {
           maxtime -= 1;
